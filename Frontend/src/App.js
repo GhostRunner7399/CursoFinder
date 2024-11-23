f2c16974fb37df424e64b6b9d77f11315730b784
halleyye <hicastro@uamv.edu.ni>
2024-11-22 20:37:25 -0600

    pantalla login lista

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,5 +1,5 @@
-import logo from './icon2.svg';
-import logoUAM from './logoUAM.svg';
+import logo from './images/icon2.svg';
+import logoUAM from './images/logoUAM.svg';
 import './App.css';
 
 function App() {
@@ -10,14 +10,16 @@ function App() {
         {/*logo uam*/}
         <img src={logoUAM} className="App-logoUAM" alt="logoUAM"/>
         {/*business person icon*/}
-        <img src={logo} className="App-logo" alt="logo" />
-        {/*creacion de rectangulos*/} 
+        <img src={logo} className="App-iconPerson" alt="iconPerson" />
+        {/*creacion de botones username password*/} 
         <div className="container">
-    <input type="text" placeholder="Username" className="input-rectangle"/>
-    <input type="password" placeholder="Password" className="input-rectangle"/>
+    <input type="text" placeholder="usuario" className="input-rectangle"/>
+    <input type="password" placeholder="contraseña" className="input-rectangle"/>
         </div>
+        {/*boton iniciar sesion*/ }
+        <button className="button-iniciarSesion">iniciar sesión</button>
         <p>
-          ESTA ES UNA PRUEBA CREO QUE ACA PONDRIAMOS EL COPYRIGHT AUNQUE NI SE PARA Q SIRVE
+        Copyright © Universidad Americana. Reservados todos los derechos.
         </p>
       </header>
     </div>
