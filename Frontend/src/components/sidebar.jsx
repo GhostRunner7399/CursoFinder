6818438602463ec673e9fd6942b26a1071b93457
stacyc03 <stacynicollecerdaruiz@gmail.com>
2024-12-05 18:04:16 -0600

    otra vez

--- Frontend/src/components/sidebar.jsx
+++ Frontend/src/components/sidebar.jsx
@@ -3,14 +3,11 @@ import styles from './sidebar.module.css';
 import settingsIcon from '../images/settings.svg';
 import statisticIcon from '../images/statistic_icon.svg';
 
-function Sidebar({ isOpen, toggleSidebar, role }) {
+function Sidebar({ isOpen, toggleSidebar }) {
   return (
-    <div className={`${styles.sidebar} ${isOpen ? styles.open : ''}`} onClick={toggleSidebar}>
+    <div className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
       <div className={styles.sidebarContent}>
-        {/* Mostrar el ícono de estadísticas solo si el rol es 'administrador' */}
-        {role === 'administrador' && (
-          <img src={statisticIcon} alt="Estadísticas" className={styles.statisticIcon} />
-        )}
+        <img src={statisticIcon} alt="Estadisticas" className={styles.statisticIcon} />
         <img src={settingsIcon} alt="Configuración" className={styles.settingsIcon} />
       </div>
     </div>
