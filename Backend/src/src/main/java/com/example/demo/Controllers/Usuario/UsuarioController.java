89442c6f291f1e749924bac736432022e5f99f1d
Andy Daivari Balladares Reyes <72000146+GhostRunner7399@users.noreply.github.com>
2025-04-08 16:19:24 -0600

    Controladores de Rol añadidos
    Ahora los usuarios pediran un Idrol ya asignado anteriormente mediante estos controllers

--- Backend/src/src/main/java/com/example/demo/Controllers/Usuario/UsuarioController.java
+++ Backend/src/src/main/java/com/example/demo/Controllers/Usuario/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.example.demo.Controllers.Usuario;
 
+import com.example.demo.DataTransferObject.Usuario.UsuarioDTO;
 import com.example.demo.Repository.Usuario.Class.Usuario;
 import com.example.demo.Services.Usuario.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +20,11 @@ public class UsuarioController {
 
     // Registrar usuario
     @PostMapping("/action/register")
-    public Usuario registrarUsuario(@RequestBody Usuario usuario) {
-        return usuarioServicio.registrarUsuario(usuario);
+    public Usuario registrarUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        return usuarioServicio.registrarUsuarioDesdeDTO(usuarioDTO);
     }
 
     // Autenticar usuario
-
     @PostMapping("/auth")
     public boolean autenticarUsuario(@RequestBody Usuario usuario) {
         return usuarioServicio.autenticarUsuario(usuario.getCif(), usuario.getContraseña());
